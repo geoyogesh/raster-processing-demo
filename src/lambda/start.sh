c161ad95cc65aba1cfbb1bc444ca0a740748b53b
Yogesh Dhanapal <geoyogesh@gmail.com>
2018-08-06 20:43:10 -0400

    bug fix

--- src/lambda/start.sh
+++ src/lambda/start.sh
@@ -3,9 +3,11 @@
 __create_user() {
 # Create a user to SSH into as.
 useradd user
-SSH_USERPASS=newpass
+SSH_USERPASS=password
 echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin root)
 echo ssh user password: $SSH_USERPASS
+echo ssh root password: $SSH_USERPASS
 }
 
 # Call all functions
